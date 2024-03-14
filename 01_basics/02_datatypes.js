ebd8c2cdaea4dd719ad9420435002ae3cefadd67
Priangshu Das <88083475+priangshudas@users.noreply.github.com>
2024-03-14 19:43:21 +0000

    learning datatypes ,null characterstic,typeof

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") 
+//we are using nodejs not browser for run the code
+
+let name = "priangshu" //string
+let age = 21 //int
+let isLoggedIn = false //bollean
+let response = null //null - datatype + standalone value
+let girlfriend //undefined - if the value is not defined
+//symbols - unique
+
+//object (most importent aka "Baaaaap")
+
+console.table([typeof null,typeof age,typeof girlfriend])//use "typeof" to log datatype
