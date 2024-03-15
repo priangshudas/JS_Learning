6a874bc0791cde1829e27f9d3ee53c102cafd608
Priangshu Das <daspriangshu99@gmail.com>
2024-03-15 20:16:55 +0530

    stack and heap memory and its working in datatypes copy vs ref.

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -40,3 +40,27 @@ myFunction("Priangshu function"); //function called
 //object (most importent aka "Baaaaap")
 
 console.table([typeof null, typeof age, typeof girlfriend]); //use "typeof" to log datatype
+
+/* Memories - 1. Stack(Primitive) it returns a copy of the datatype
+              2. Heap(Non-primitive) it returns a reference of the datatype
+*/
+
+let friend = "soumya";
+let anotherFriend = friend;
+anotherFriend = "subho";
+
+console.log(friend);
+console.log(anotherFriend);
+//above is the example of stack memory
+
+let friendDetails = {
+  name: "pijus",
+  age: 18,
+};
+let anotherFriendDetails = friendDetails;
+
+anotherFriendDetails.age = 21;
+
+console.log(friendDetails);
+console.log(anotherFriendDetails);
+//above is the example of heap memory
